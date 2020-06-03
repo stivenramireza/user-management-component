e5628f075ac11875b817bca851e70f8689853fd9
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-06-02 21:48:40 -0500

    Component finished

--- user_management_component.py
+++ user_management_component.py
@@ -5,15 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 from passlib.apps import custom_app_context as pass_lib
 from itsdangeorus import JSONWebSignatureSerializer, BadSignature, SignatureExpired
 
-"""
-    Configuración de la API en Flask
-"""
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '3df9bbf3-1a43-43db-9ebb-f395cf555491')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 
 db = SQLAlchemy(app)
 token_serializer = JSONWebSignatureSerializer(app.config['SECRET_KEY'])
@@ -24,36 +23,31 @@ user_role_table = db.Table(
     db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
 )
 
-"""
-    Modelo de usuario
-"""
+
 class User(db.Model):
 
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(32), index=True, unique=True)
-    password = db.Column(db.String(64))
-    is_admin = db.Column(db.Boolean)
+    password = db.Column(db.String(64), nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False)
+    
+    name = db.Column(db.String(64), nullable=False)
+    comercial_title = db.Column(db.String(64))
+    company_name = db.Column(db.String(64))
+    position = db.Column(db.String(64))
+    email = db.Column(db.String(64), nullable=False)
+    phone = db.Column(db.String(64), nullable=False)
+
     roles = db.relationship('Role', secondary=user_role_table, backref=db.backref('users', lazy='dynamic'))
 
-    """
-        Envía la contraseña encriptada
-        :password Contraseña a encriptar
-    """
     def set_password(self, password):
         self.password = pass_lib.encrypt(password)
 
-    """
-        Autenticar contraseña
-        :password Contraseña que se va a autenticar
-    """
     def authenticate(self, password):
         return pass_lib.verify(password, self.password)
 
-    """
-        Convertir la información del usuario en un diccionario para manejar JSON
-    """
     def to_dict(self):
         return {
             'id': self.id,
@@ -64,16 +58,10 @@ class User(db.Model):
             ]
         }
 
-    """
-        Obtener el token con el cual el usuario se va a autenticar
-        :password Contraseña a encriptar
-    """
     def get_token(self):
         return token_serializer.dumps(self.to_dict())
 
-"""
-    Modelo de Rol
-"""
+
 class Role(db.Model):
 
     __tablename__ = 'role'
@@ -81,19 +69,13 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
 
-    """
-        Convertir la información del rol en un diccionario para manejar JSON
-    """
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
         }
 
-"""
-    Verificar que el token enviado sea válido
-    :token Token que se va a validar
-"""
+
 def validate_token(token):
     try:
         data = token_serializer.loads(token)
@@ -102,9 +84,7 @@ def validate_token(token):
 
     return data
 
-"""
-    Endpoint que permite la creación de un usuario en la base de datos
-"""
+
 @app.route('/api/user', methods=['POST'])
 def create_user_api():
     user_name = request.json.get('user_name')
@@ -126,9 +106,7 @@ def create_user_api():
 
     return jsonify(user.to_dict), 200
 
-"""
-    Endpoint que permite la creación de un rol para un usuario en la base de datos
-"""
+
 @app.route('/api/user/add_role', methods=['POST'])
 def add_role_api():
     token = request.json.get('token')
@@ -147,9 +125,7 @@ def add_role_api():
 
     return jsonify(user.to_dict), 200
 
-"""
-    Endpoint que permite la autenticación de un usuario
-"""
+
 @app.route('/api/authenticate', methods=['POST'])
 def authenticate_api():
     user_name = request.json.get('user_name')
@@ -172,9 +148,7 @@ def authenticate_api():
         'auth_token': user.get_token()
     }), 200
 
-"""
-    Endpoint que le permite a un usuario acceder a los recursos autorizados
-"""
+
 @app.route('/api/autorize', method=['POST'])
 def authorize_api():
     token = request.json.get('auth_token')
